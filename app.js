906fb5c0f55eea3c749b6f3ddfe37e30192b853a
bublinkac <bublinkac@gmail.com>
2017-06-02 15:07:04 +0200

    commit1

--- app.js
+++ app.js
@@ -2,35 +2,28 @@ const express = require('express')
 const bodyParser= require('body-parser')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
+var routes = require('./routes')
+//, user = require('./routes/user')
+, http = require('http')
+, path = require('path');
 
 var db
 
-app.use(bodyParser.urlencoded({extended: true}))
 //app.set('view engine', 'ejs')
 app.set('view engine', 'jade')
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
 
-app.get('/', function(req, res) {
-  //res.sendFile(__dirname + '/index.html')
-  db.collection('quotes').find().toArray(function(err, results) {
-  //console.log(results)
-  res.render('index.ejs', {quotes: results})
-  })
-})
 
-app.post('/quotes', (req, res) => {
-  db.collection('quotes').save(req.body, (err, result) => {
-    if (err) return console.log(err)
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended: true}))
 
-    console.log('saved to database')
-    res.redirect('/')
-  })
-})
-
+app.get('/', routes.index);
 
 MongoClient.connect('mongodb://localhost:27017/test', (err, database) => {
   if (err) return console.log(err)
   db = database
   app.listen(3000, () => {
-    console.log('listening on 3000')
+    console.log('listening on 3000 aaha')
   })
 })
